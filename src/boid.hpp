0c063e7c4339ca01bd13543a0bc93712cd2646d9
Carl-Victor Schauman <schaumc1@aalto.fi>
2020-07-22 16:05:48 +0300

    Added some attributes to boid.hpp, some lines need to be uncommented for vector3D

--- src/boid.hpp
+++ src/boid.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
-class Boid {
+class Boid
+{
 public:
     Boid();
     ~Boid();
+
+private:
+    float mass;
+    //vector3D position;
+    int positionIndex;
+    //vector3D nextPosition;
+    int nextPositionIndex;
+    //vector3D velocity;
+    float maxForce;
+    float maxSpeed;
+    //vector3D orientation;
 };
