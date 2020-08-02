fee5d0b13805c20f4477ee83cfe8670890e303ec
Jan Nyberg <jan.nyberg@gmail.com>
2020-08-02 11:49:58 -0700

    refactored shader handlers by creating shaderfactory class

--- src/main.cpp
+++ src/main.cpp
@@ -29,77 +29,7 @@
 #include "world.hpp"
 #include "vector3d.hpp"
 #include "boid.hpp"
-
-struct ShaderSources {
-	std::string VertexSource;
-	std::string FragmentSource;
-};
-
-static ShaderSources shaderParser(const std::string& filepath) {
-	std::ifstream stream(filepath);
-
-	enum class ShaderType
-	{
-		NONE = -1, VERTEX = 0, FRAGMENT = 1
-	};
-
-	std::string line;
-	std::stringstream ss[2];
-	ShaderType type = ShaderType::NONE;
-	while (getline(stream, line)) {
-		if (line.find("#shader") != std::string::npos) {
-			if (line.find("vertex") != std::string::npos) {
-				type = ShaderType::VERTEX;
-			}
-			else if (line.find("fragment") != std::string::npos) {
-				type = ShaderType::FRAGMENT;
-			}
-		} else {
-			ss[(int)type] << line << '\n';
-		}
-	}
-	return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& source)
-{
-	unsigned int id = glCreateShader(type);
-	const char* src = source.c_str();
-	glShaderSource(id, 1, &src, nullptr);
-	glCompileShader(id);
-
-	int result;
-	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE) {
-		int length;
-		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*)alloca(length * sizeof(char));
-		glGetShaderInfoLog(id, length, &length, message);
-		std::cout << "Failed to complie " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
-		std::cout << message << std:: endl;
-		glDeleteShader(id);
-		return 0;
-	}
-
-	return id;
-}
-
-unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-	unsigned int program = glCreateProgram();
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glValidateProgram(program);
-
-	glDeleteShader(vs);
-	glDeleteShader(fs);
-
-	return program;
-}
+#include "shader.hpp"
 
 class MyApp: public wxApp
 {
@@ -223,14 +153,14 @@ void wxGLCanvasSubClass::Render()
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
-	ShaderSources source = shaderParser("boid.shader");
+	static auto sf = ShaderFactory("boid.shader");
 
 	// std::cout << "Vertex" << std::endl;
-	// std::cout << source.VertexSource << std::endl;
+	// std::cout << sf.GetVertexShader() << std::endl;
 	// std::cout << "Fragment" << std::endl;
-	// std::cout << source.FragmentSource << std::endl;
+	// std::cout << sf.GetFragmentShader() << std::endl;
 
-	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	unsigned int shader = sf.CreateShader();
 
 	glUseProgram(shader);
 
